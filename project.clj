66e9f67068bcf67a4e34af436bb2131427439b48
Oshoma Momoh <oshoma@gmail.com>
2016-05-11 12:30:07 -0400

    Upgrade to 1.6.0

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jdom/jdom "2.0.2"]
-                 [com.rometools/rome "1.5.1"]])
+                 [com.rometools/rome "1.6.0"]
+                 [com.rometools/rome-modules "1.6.0"]])
