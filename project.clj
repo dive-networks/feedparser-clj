621fdb6a44f7678e0c4e09ac4aa8c3a0a5637ba9
Dominik Magdaleński <dominik.magdalenski@idego.pl>
2019-11-21 10:19:10 +0100

    Bump rome from 1.6.0 to 1.12.2

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[base64-clj "0.1.1"]
                  [org.clojure/clojure "1.8.0"]
                  [org.jdom/jdom "2.0.2"]
-                 [com.rometools/rome "1.6.0"]
-                 [com.rometools/rome-modules "1.6.0"]])
+                 [com.rometools/rome "1.12.2"]
+                 [com.rometools/rome-modules "1.12.2"]])
