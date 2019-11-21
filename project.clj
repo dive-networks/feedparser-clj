a611b405569684d5ae7587b44ba9c618b5ddad8d
Dominik Magdaleński <dominik.magdalenski@idego.pl>
2019-11-21 10:24:36 +0100

    Bump clojure from 1.8.0 to 1.10.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.dive-networks/feedparser-clj "0.5.3"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[base64-clj "0.1.1"]
-                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.10.1"]
                  [org.jdom/jdom "2.0.2"]
                  [com.rometools/rome "1.12.2"]
                  [com.rometools/rome-modules "1.12.2"]])
